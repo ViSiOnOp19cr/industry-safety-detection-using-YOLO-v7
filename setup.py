d18e0676eed20dc875c156515521375acd105f2a
chandan cr <chandancr515@gmail.com>
2024-04-21 00:29:54 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'chandan cr',
+    author_email= 'chandancr98195@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
